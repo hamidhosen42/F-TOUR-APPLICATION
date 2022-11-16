29af18b9d25d8127659ec2431bb214a3183321f7
hamidhosen42 <mdhamidhosen8444@gmail.com>
2022-11-16 19:17:32 +0600

    add-main-page

---
+++ lib/screen/splash_screen.dart
@@ -0,0 +1,15 @@
+// ignore_for_file: prefer_const_constructors
+import 'package:flutter/material.dart';
+
+class SplashScree  extends StatelessWidget {
+  const SplashScree ({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: Text("Hello"),
+      ),
+    );
+  }
+}
